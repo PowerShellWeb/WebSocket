3fd880eafc3a776f433d6a736a7c29a22b5a7193
James Brundage <+@noreply.github.com>
2024-11-27 23:33:34 -0800

    feat: WebSocket action ( Fixes #24 )

---
+++ Build/WebSocket.GitHubAction.PSDevOps.ps1
@@ -0,0 +1,10 @@
+﻿#requires -Module PSDevOps
+Import-BuildStep -SourcePath (
+    Join-Path $PSScriptRoot 'GitHub'
+) -BuildSystem GitHubAction
+
+$PSScriptRoot | Split-Path | Push-Location
+
+New-GitHubAction -Name "UseWebSocket" -Description 'Work with WebSockets in PowerShell' -Action WebSocketAction -Icon chevron-right -OutputPath .\action.yml
+
+Pop-Location
